7868ecc5819c900a4728e916370f7ff85994bbfb
lukaszbros <lukasz.bros@gmail.com>
2021-08-06 12:37:51 +0200

    Introduce planets reducer and fetch status error handling

---
+++ src/components/Planets/planetsReducer.js
@@ -0,0 +1,64 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios';
+
+export const fetchPlanets = createAsyncThunk('planets/fetchPlanets', async () => {
+  const response = await axios.get('https://swapi.dev/api/planets/');
+  return response.planets;
+});
+
+const initialState = {
+  planets: [],
+  status: 'idle',
+  error: null
+}
+
+const postsSlice = createSlice({
+  name: 'planets',
+  initialState,
+  reducers: {
+    /*postAdded: {
+      reducer(state, action) {
+        state.posts.push(action.payload)
+      },
+      prepare(title, content, userId) {
+        // omit prepare logic
+      }
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.posts.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.posts.find(post => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    }*/
+  },
+  extraReducers: {
+    [fetchPlanets.pending]: (state, action) => {
+      state.status = 'loading'
+    },
+    [fetchPlanets.fulfilled]: (state, action) => {
+      state.status = 'succeeded'
+      // Add any fetched posts to the array
+      state.planets = state.planets.concat(action.payload)
+    },
+    [fetchPlanets.rejected]: (state, action) => {
+      state.status = 'failed'
+      state.error = action.error.message
+    }
+  }
+})
+
+/*export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions*/
+
+export default postsSlice.reducer;
+
+export const selectAllPlanets = state => state.planets.planets;
+
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './components/App/counterReducer';
+import planetsReducer from './components/Planets/planetsReducer';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    planets: planetsReducer
   },
 })
